bf554eff23f9ce4ccf8c96fa961a4397da74f59f
Marco Scarselli <scarselli@gmail.com>
2022-12-14 10:21:28 +0100

    added localStorage to save Hyper Transcripts and be able to resume transcription work

---
+++ js/hyperadio-lite-editor-localstorage.js
@@ -0,0 +1,81 @@
+/*
+ * HyperTranscriptStorage class
+ * @param {string} hypertranscript - the html of the hypertranscript
+ * @param {string} video - the html of the video
+ * @return {void}
+ */
+class HyperTranscriptStorage {
+  constructor(hypertranscript, video) {
+    this.hypertranscript = hypertranscript;
+    this.video = video;
+  }
+}
+/*
+ * Save the current HyperTranscript in the local storage
+ * @param {string} transcriptionName - the name of the transcription
+ * @param {string} hypertranscriptDomId - the id of the hypertranscript dom element
+ * @param {string} videoDomId - the id of the video dom element
+ * @return {void}
+ */
+function renderTranscript(
+  hypertranscriptstorage,
+  hypertranscriptDomId = 'hypertranscript',
+  videoDomId = 'hyperplayer',
+) {
+  document.getElementById(hypertranscriptDomId).innerHTML = hypertranscriptstorage['hypertranscript'];
+  document.getElementById(videoDomId).innerHTML = hypertranscriptstorage['video'];
+}
+
+function saveHyperTranscript(
+  transcriptionName = 'hypertranscript--last',
+  hypertranscriptDomId = 'hypertranscript',
+  videoDomId = 'hyperplayer',
+) {
+  let hypertranscript = document.getElementById(hypertranscriptDomId).innerHTML;
+  let video = document.getElementById(videoDomId).innerHTML;
+  let hypertranscriptstorage = new HyperTranscriptStorage(hypertranscript, video);
+  localStorage.setItem(transcriptionName, JSON.stringify(hypertranscriptstorage));
+  console.log('HyperTranscript saved');
+}
+
+/*
+ * Load the current HyperTranscript in the local storage
+ * @param {string} transcriptionName - the name of the transcription
+ * @param {string} hypertranscriptDomId - the id of the hypertranscript dom element
+ * @param {string} videoDomId - the id of the video dom element
+ * @return {void}
+ */
+function loadHyperTranscript(
+  transcriptionName = 'hypertranscript--last',
+  hypertranscriptDomId = 'hypertranscript',
+  videoDomId = 'hyperplayer',
+) {
+  let hypertranscriptstorage = JSON.parse(localStorage.getItem(transcriptionName));
+  if (hypertranscriptstorage) {
+    renderTranscript(hypertranscriptstorage);
+    console.log('HyperTranscript loaded');
+  } else {
+    alert('no saved HyperTranscript found');
+  }
+}
+
+/*
+ * Select the HyperTranscript in the local storage to display
+ */
+function selectLoadHyperTranscript() {
+  let hypertranscriptSavedUrls = '';
+  for (let i = 0; i < localStorage.length; i++) {
+    hypertranscriptSavedUrls += `\n ${i} - ${localStorage.key(i)}`;
+  }
+  hypertranscriptSavedUrls += '';
+  console.log(hypertranscriptSavedUrls);
+
+  let transcriptionNameKey = prompt(`Enter the url of the saved HyperTranscript: ${hypertranscriptSavedUrls}`);
+  let hypertranscriptstorage = JSON.parse(localStorage.getItem(localStorage.key(transcriptionNameKey)));
+  if (hypertranscriptstorage) {
+    renderTranscript(hypertranscriptstorage);
+    console.log('HyperTranscript loaded');
+  } else {
+    alert('no saved HyperTranscript found');
+  }
+}
